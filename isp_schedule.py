e88c1c511f764a1cb7d53074e76fa58b6eb88e08
Soubhik Sinhababu <me@soubhik.com>
2016-02-29 03:02:16 +0530

    Added new method for initial logging in while running the isp_login script.

--- isp_schedule.py
+++ isp_schedule.py
@@ -6,8 +6,9 @@ import time
 
 def job():
     print("Wait, while I search if you're logged in or not! ...")
-    subprocess.call(["python","login.py"])
+    subprocess.call(["python","isp_login.py"])
 
+subprocess.call(["python", "isp_login.py"]) # Initial login.
 schedule.every(15).minutes.do(job) # Schedule the login script to do its job every 15 minutes.
 
 while True:
